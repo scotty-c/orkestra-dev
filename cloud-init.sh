b73962447f7bf6a676603ef5e92691fd6d2033b4
scott coulton <scott.coulton@microsoft.com>
2021-10-05 12:31:29 +1100

    adding argo

--- cloud-init.sh
+++ cloud-init.sh
@@ -5,13 +5,15 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "# make..."
 sudo apt-get install -y \
-        make
+        make \
+        gzip
 
 echo "# microk8s..."
 sudo snap install microk8s --classic --channel=1.21
 mkdir -p $HOME/.kube/
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
+
 sudo microk8s config > $HOME/.kube/config
 sudo microk8s enable dns
 
@@ -25,6 +27,13 @@ source ~/.bash_aliases
 
 echo " # Helm..."
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+echo " # argo..."
+# Download the binary
+curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.2.0-rc5/argo-linux-amd64.gz
+gzip argo-linux-amd64.gz
+chmod +x argo-linux-amd64
+mv ./argo-linux-amd64 /usr/local/bin/argo
 
 echo "# orkestra..."
 git clone https://github.com/Azure/orkestra.git
